dfb35fdf021b819d64bc38011397977d7d7965cd
guillaume <salicisguillaume@yahoo.fr>
2018-07-21 22:57:11 +0200

    feature: backward, pause and resume

--- index.js
+++ index.js
@@ -4,52 +4,54 @@ const { split } = require('./lib/split')
 const { pivot } = require('./lib/pivot')
 const BluPromise = require('bluebird')
 
-function genepiBuilder(text, outputter, delayMs = 200) {
-  return (resolve, reject, onCancel) => {
-    outputter.header()
-    const words = split(text)
-    const time = setInterval(() => {
-      if (words.length < 1) {
-        clearInterval(time)
-        outputter.footer()
-        resolve()
-      } else {
-        const word = words.shift()
-        const index = pivot(word)
-        outputter.inner(word, index)
-      }
-    }, delayMs)
-    onCancel(() => clearInterval(time))
-  }
-}
+BluPromise.config({
+  cancellation: true
+})
 
 class GenepiReader {
   constructor() {
     this._delay = 200
     this._currentIndex = 0
+    this._backward = false
     this._prom = null
   }
 
   pause() {
     // bluebird cancel is synchronous
-    // this._prom.cancel();
+    this._prom.cancel()
+    return this._currentIndex
   }
 
-  play(text, outputter, delayMs = 200) {
-    this._prom = new BluPromise(
-      genepiBuilder(text, outputter, delayMs = 200))
+  resume(text, outputter, backward = false) {
+    return this.play(text, outputter, this._delay, this._currentIndex, backward)
+  }
+
+  play(text, outputter, delayMs = 200, position = 0, backward = false) {
+    this._delay = delayMs
+    this._currentIndex = position
+    // TODO avoid duplication
+    this._prom = new BluPromise((resolve, reject, onCancel) => {
+      outputter.header()
+      const words = split(text)
+      const time = setInterval(() => {
+        if (this._prom.isCancelled()) return // TODO un-hack
+        if ((this._currentIndex < 0) || (words.length <= this._currentIndex)) {
+          clearInterval(time)
+          outputter.footer()
+          resolve()
+        } else {
+          const word = words[this._currentIndex]
+          const index = pivot(word)
+          outputter.inner(word, index)
+        }
+        this._currentIndex = this._currentIndex + (backward ? -1 : 1)
+      }, this._delay)
+      // actually this line is tested, istanbul does not see it. TODO see why
+      /* istanbul ignore next */
+      onCancel && onCancel(() => clearInterval(time))
+    })
     return this._prom
   }
-
-  backward() { }
-
-  get promise() {
-    return this._prom
-  }
-
-  set delay(duration) { }
-
-  get delay() {}
 }
 
 /**
@@ -60,7 +62,24 @@ class GenepiReader {
  * @param {number} delayMs delay between two calls to outputter.inner(word, index)
  */
 function genepi(text, outputter, delayMs = 200) {
-  return new Promise(genepiBuilder(text, outputter, delayMs))
+  // TODO avoid duplication
+  return new Promise((resolve) => {
+    let wordIndex = 0
+    outputter.header()
+    const words = split(text)
+    const time = setInterval(() => {
+      if ((wordIndex < 0) || (words.length <= wordIndex)) {
+        clearInterval(time)
+        outputter.footer()
+        resolve()
+      } else {
+        const word = words[wordIndex]
+        const index = pivot(word)
+        outputter.inner(word, index)
+      }
+      wordIndex++
+    }, delayMs)
+  })
 }
 
 module.exports = { genepi, GenepiReader }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -39,12 +39,13 @@ describe('Genepi', () => {
         expect(spyHeader).toHaveBeenCalled()
         expect(spyInner).toHaveBeenCalled()
         expect(spyFooter).toHaveBeenCalled()
-        expect(spyInner).toHaveBeenCalled()
-        expect(spyInner).toBeCalledWith('This', 1)
-        expect(spyInner).toBeCalledWith('is', 1)
-        expect(spyInner).toBeCalledWith('a', 0)
-        expect(spyInner).toBeCalledWith('splendid', 2)
-        expect(spyInner).toBeCalledWith('textstring.', 3)
+        expect(spyInner.mock.calls).toEqual([
+          ['This', 1],
+          ['is', 1],
+          ['a', 0],
+          ['splendid', 2],
+          ['textstring.', 3]
+        ])
       })
     })
     it('can play forward from a given word index', () => {
@@ -55,16 +56,30 @@ describe('Genepi', () => {
         expect(spyHeader).toHaveBeenCalled()
         expect(spyInner).toHaveBeenCalled()
         expect(spyFooter).toHaveBeenCalled()
-        expect(spyInner).toHaveBeenCalled()
-        expect(spyInner).not.toBeCalledWith('This', 1)
-        expect(spyInner).not.toBeCalledWith('is', 1)
-        expect(spyInner).toBeCalledWith('a', 0)
-        expect(spyInner).toBeCalledWith('splendid', 2)
-        expect(spyInner).toBeCalledWith('textstring.', 3)
+        expect(spyInner.mock.calls).toEqual([
+          ['a', 0],
+          ['splendid', 2],
+          ['textstring.', 3]
+        ])
       })
     })
-    it('can play backward', () => {})
-    it('can change delay', () => {})
+    it('can play backward', () => {
+      const reader = new GenepiReader()
+      const text = 'This is a splendid textstring.'
+
+      return reader.play(text, outputter, 1, 3, -1).then(() => {
+        expect(spyHeader).toHaveBeenCalled()
+        expect(spyInner).toHaveBeenCalled()
+        expect(spyFooter).toHaveBeenCalled()
+        expect(spyInner).not.toBeCalledWith('textstring.', 3)
+        expect(spyInner.mock.calls).toEqual([
+          ['splendid', 2],
+          ['a', 0],
+          ['is', 1],
+          ['This', 1]
+        ])
+      })
+    })
   })
 
   describe('genepi function iterates on a string containing words', () => {
@@ -115,6 +130,40 @@ describe('Genepi', () => {
 
       jest.advanceTimersByTime(200)
       expect(spyFooter).toBeCalled()
+    })
+  })
+  describe('GenepiReader advanced', () => {
+    it('can be paused and resumed', () => {
+      jest.useFakeTimers()
+      const text = 'This is a splendid textstring.'
+
+      const reader = new GenepiReader()
+      reader.play(text, outputter)
+
+      expect(spyInner).not.toBeCalled()
+
+      jest.advanceTimersByTime(400)
+      expect(spyInner.mock.calls).toEqual([
+        ['This', 1],
+        ['is', 1]
+      ])
+      const currentIndex = reader.pause()
+      expect(currentIndex).toBe(2)
+      jest.advanceTimersByTime(1000)
+
+      expect(spyInner.mock.calls).toEqual([
+        ['This', 1],
+        ['is', 1]
+      ])
+      reader.resume(text, outputter)
+      jest.advanceTimersByTime(1000)
+      expect(spyInner.mock.calls).toEqual([
+        ['This', 1],
+        ['is', 1],
+        ['a', 0],
+        ['splendid', 2],
+        ['textstring.', 3]
+      ])
     })
   })
 })
